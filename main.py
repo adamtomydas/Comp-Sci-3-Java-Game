48a0dc63be485b1d0486395a26db2ba5f3f3385a
Adam Tomydas <adamtomydas@gmail.com>
2024-10-25 16:18:24 -0500

    pygame initial

--- main.py
+++ main.py
@@ -3,13 +3,28 @@ from hand import *
 
 import ctypes, pygame, sys
 
+pygame.init()
+SCREEN_WIDTH = 800
+SCREEN_LENGTH = 800
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_LENGTH))
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
+
+
 
 
 def Game():
     print()
 
 
-
+"""
 if __name__ == "__main__":
     game = Game()
     game.run()
+    """
